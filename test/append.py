ceb984b5189baf83476e2ad48bfcd9172381bfc1
dunkyfool <dunkyfool0113@gmail.com>
2016-07-28 10:10:29 +0800

    update

--- test/append.py
+++ test/append.py
@@ -3,6 +3,7 @@ import h5py
 
 a = np.array([1,2,3,4,5,2,3,4,5,6]).reshape((2,5))
 b = np.array([6,7,8,9,10]).reshape((1,5))
+c = np.ones((4,2))
 with h5py.File('tmp','w') as hf:
   hf.create_dataset('0',(0,0),compression='gzip',maxshape=(None,None))
   hf.create_dataset('1',(1,5),compression='gzip')
@@ -18,8 +19,10 @@ with h5py.File('tmp','a') as hf:
   hf['0'].resize((a.shape))
   print hf['0'].shape
   hf['0'][...] = a
-  hf['0'].resize((4,5))
-  hf['0'][3:4,:]=b
+  hf['0'].resize((4,7))
+  hf['0'][3:4,0:5]=b
+
+  hf['0'][...] = np.concatenate([hf['0'][:,0:5],c],axis=1)
 
 with h5py.File('tmp','r') as hf:
   z = np.array(hf['0'])
